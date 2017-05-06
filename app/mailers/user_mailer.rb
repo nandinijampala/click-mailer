af1c9b8d10d6099eec010a8a2913d9ec2ab259c4
nandini jampala <nandinijampala3@gmail.com>
2017-05-06 14:30:40 +0530

    init

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "yogeshwer235@gmail.com"
+  layout 'mailer'
+end
+
+class UserMailer < ApplicationMailer
+ 
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
